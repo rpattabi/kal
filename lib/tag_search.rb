9d6e1669eccd343b5a11135f9750a969e5803443
rpattabi <ragunathan.pattabiraman@gmail.com>
2012-09-05 22:50:37 +0530

    TagSearch constructor is improved with proper exceptions

--- lib/tag_search.rb
+++ lib/tag_search.rb
@@ -1,10 +1,46 @@
 
-class TagSearch
-  attr_accessor :tags
+module Kal
+  module Exceptions
+    class BadTagError < Exception
+    end
 
-  def initialize(tags)
-    @tags = []
-    @tags << tags
-    @tags.flatten!
+    class BadRootPathError < Exception
+    end
   end
 end
+
+class TagSearch
+  include Kal::Exceptions
+
+  attr_accessor :tags
+  attr_reader :root
+
+  def initialize(tags, root_path='./')
+    @tags = process_tags(tags)
+    @root = process_root(root_path)
+  end
+
+  private
+
+  def process_tags(tags)
+    tmp_tags = []
+    tmp_tags << tags
+    tmp_tags.flatten!
+
+    processed_tags = tmp_tags.collect { |t| t.strip unless t.strip.empty? }
+    processed_tags.compact!
+    raise BadTagError if processed_tags.empty?
+
+    processed_tags
+  end
+
+  def process_root(root_path)
+    raise BadRootPathError unless File.exists?(root_path)
+    raise BadRootPathError unless File.directory?(root_path)
+
+    root_path
+  end
+end
+
+
+
--- test/test_tag_search.rb
+++ test/test_tag_search.rb
@@ -1,17 +1,25 @@
-require './lib/tag_search'
+require '../lib/tag_search'
+require 'tempfile'
 require 'test/unit'
 
 class TestTagSearch < Test::Unit::TestCase
+  include Kal::Exceptions
+
   def setup
 
   end
 
   def test_initialize
     assert_raise(ArgumentError) { TagSearch.new() }
+    assert_raise(BadTagError) { TagSearch.new('') }
+    assert_raise(BadTagError) { TagSearch.new('  ') }
+    assert_raise(BadTagError) { TagSearch.new([]) }
+    assert_raise(BadTagError) { TagSearch.new(['', ' ']) }
 
     search = TagSearch.new('kalyani')
     assert_not_nil(search)
     assert_equal(['kalyani'], search.tags)
+    assert_equal('./', search.root)
   end
 
   def test_tags
@@ -20,5 +28,25 @@ class TestTagSearch < Test::Unit::TestCase
 
     search = TagSearch.new(['anu', 'ragu'])
     assert_equal(['anu', 'ragu'], search.tags)
+
+    search = TagSearch.new(['anu', '', 'ragu'])
+    assert_equal(['anu', 'ragu'], search.tags)
+
+    search = TagSearch.new(['anu', 'ragu', '  '])
+    assert_equal(['anu', 'ragu'], search.tags)
+  end
+
+  def test_root
+    search = TagSearch.new('ragu')
+    assert_equal('./', search.root)
+
+    search = TagSearch.new('ragu', '/tmp')
+    assert_equal('/tmp', search.root)
+
+    assert_raise(BadRootPathError) { search = TagSearch.new('ragu', '/tmp/dingdong/') }
+
+    Tempfile.open('dingdong') do |f|
+      assert_raise(BadRootPathError) { search = TagSearch.new('ragu', f.path) }
+    end
   end
 end
